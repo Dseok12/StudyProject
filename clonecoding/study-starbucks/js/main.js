c7190bb9d04027e591081d579525a51e1325712a
D.seok <ehskstkrjsa@naver.com>
2021-07-13 22:51:06 +0900

    STUDY: CloneCodung-StarBucks-Badges

--- clonecoding/study-starbucks/js/main.js
+++ clonecoding/study-starbucks/js/main.js
@@ -14,7 +14,26 @@ searchInputEl.addEventListener('blur', function(){
     searchInputEl.setAttribute('placeholder', '');
 });
 
+// 스크롤시 특정 요소 사라지게하기
 
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+    console.log(window.scrollY)
+    if (window.scrollY > 500){
+        // 배지 숨기기
+        // gsap.to(요소, 지속시간, {옵션});
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+    } else {
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+}, 300));
 
 
 
