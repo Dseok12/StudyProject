4570aabe0098dca7c83ed85358e4ca18694815de
D.seok <ehskstkrjsa@naver.com>
2021-07-16 23:04:29 +0900

    STUDY: Clone Coding - Starbucks

--- clonecoding/study-starbucks/js/main.js
+++ clonecoding/study-starbucks/js/main.js
@@ -37,6 +37,14 @@ window.addEventListener('scroll', _.throttle(function(){
 
 
 
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+    // gsap.to(요소, 지속시간, {옵션});
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7,
+        opacity: 1
+    });
+});
 
 
 
