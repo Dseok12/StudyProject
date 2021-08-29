0374d7d63bcdb55c2d3a6afefdf5bf5feb6b60ac
D.seok <ehskstkrjsa@naver.com>
2021-08-29 17:58:13 +0900

    STUDY: Web-Design

--- WebDesign/practice01/16-총연습3/js/index.js
+++ WebDesign/practice01/16-총연습3/js/index.js
@@ -32,10 +32,13 @@ var tabCont = $('.tabCont > div');
 tabCont.hide().eq(0).show();
 tabBtn.click(function(e){
     e.preventDefault();
+
     var target = $(this);
     var index = target.index();
+
     tabBtn.removeClass('active');
     target.addClass('active');
+    
     tabCont.css('display', 'none');
     tabCont.eq(index).css('display', 'block');
 });
--- WebDesign/practice01/19-총연습6/js/index.js
+++ WebDesign/practice01/19-총연습6/js/index.js
@@ -1,12 +1,58 @@
+$('.nav ul li').hover(
+    function() {
+        $('.nav > ul > li > .submenu').stop().slideDown();
+    },
+    function() {
+        $('.nav > ul > li > .submenu').stop().slideUp();
+    }
+)
+
+
+
+
+// $('.slideList').children('div:gt(0)').hide();
+// var current = 0;
+
+// setInterval(function() {
+//     var next = (current + 1) % 3;
+//     $('.slideList').find('div').eq(current).fadeOut();
+//     $('.slideList').find('div').eq(next).fadeIn();
+//     current = next;
+// }, 2000);
+
+
+
+$('.slideList').children('div:gt(0)').hide();
+
+var current = 0;
+setInterval(function(){
+    var next = (current + 1) % 3;
+    $('.slideList').find('div').eq(current).fadeOut();
+    $('.slideList').find('div').eq(next).fadeIn();
+    current = next;
+},2000)
 
 
 
 
 
+var tabBtn = $('.tabMenu ul li');
+var tabCont = $('.tabCont > div');
 
+tabCont.hide().eq(0).show();
 
+tabBtn.click(function(e){
+    e.preventDefault();
 
+    var target = $(this);
+    var index = target.index();
 
+    tabBtn.removeClass('active');
+    target.addClass('active');
+
+    tabCont.css('display', 'none');
+    tabCont.eq(index).css('display', 'block');
+})
 
 
 
