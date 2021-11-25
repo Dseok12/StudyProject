c5d9f872f0f7453cbf43a0e3d845c4031ed5e5fa
D.seok <ehskstkrjsa@naver.com>
2021-11-25 15:05:27 +0900

    Step1 -> 주석 추가

---
+++ JS연습/deepdive/Step1/index.js
@@ -0,0 +1,19 @@
+/* While문 */
+
+let count = 1;
+
+// 무한루프
+while (true) {
+  console.log(count);
+  count++;
+
+  // count가 3이면(2까지) 멈춤
+  if(count === 3) break;
+}
+
+/*
+var num = '안녕';
+console.log(typeof(parseInt(num)))
+*/
+
+
