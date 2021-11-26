fc3a289e52d3a66bfa66e7e425958d4366c7dbbe
D.seok <ehskstkrjsa@naver.com>
2021-11-26 13:05:26 +0900

    Step 10 -> Promise 진행중 3

--- JS연습/deepdive/Step1/index.js
+++ JS연습/deepdive/Step1/index.js
@@ -1,19 +1,56 @@
 /* While문 */
 
-let count = 1;
+// let count = 1;
 
-// 무한루프
-while (true) {
-  console.log(count);
-  count++;
+// // 무한루프
+// while (true) {
+//   console.log(count);
+//   count++;
 
-  // count가 3이면(2까지) 멈춤
-  if(count === 3) break;
-}
+//   // count가 3이면(2까지) 멈춤
+//   if(count === 3) break;
+// }
 
 /*
 var num = '안녕';
 console.log(typeof(parseInt(num)))
 */
+
+
+/*
+  1. swich문을 이용하여 홀수, 짝수를 출력하는 프로그램을 만드세요.
+*/
+
+/*
+call by referecne
+
+const a = {
+  greeting : `Hello ${this.name}`,
+  name : 'raven'
+}
+let b;
+b = a;
+
+console.log(b);
+b.name = 'parrot'
+console.log(a);
+console.log(a === b);
+*/
+
+function changVal (primitive, obj){
+  primitive += 100;
+  obj.name = 'Kim';
+}
+
+var num = 300;
+var person = {name: 'Lee'};
+
+console.log(num);
+console.log(person);
+
+changVal(num, person);
+console.log(num);
+console.log(person);
+
 
 
