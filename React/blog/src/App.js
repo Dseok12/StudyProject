75c7b5d94dc7c73bf4a40dc7c06d6d596b3b5587
D.seok <ehskstkrjsa@naver.com>
2021-12-03 15:39:13 +0900

    react

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+
+  let posts = '강남 고기 맛집';
+
+  function 함수 (){
+    return 100;
+  }
+
+  let _css = {color: 'white', fontSize: '24px'}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕하세요.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style={_css}>개발 blog</div>
+      </div>
+      <h4>{posts}</h4>
+      <h4>{함수()}</h4>
     </div>
   );
 }
