77d95adaa6d68bf1c488fa9a2877653e70da4bd3
D.seok <ehskstkrjsa@naver.com>
2021-12-03 16:38:46 +0900

    react

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,23 +1,35 @@
+import React, { useState } from 'react';
 import './App.css';
 import logo from './logo.svg';
 
 function App() {
 
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '우동 맛집', '돈까스 맛집']);
+
   let posts = '강남 고기 맛집';
 
-  function 함수 (){
-    return 100;
-  }
 
-  let _css = {color: 'white', fontSize: '24px'}
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={_css}>개발 blog</div>
+        <div>개발 blog</div>
       </div>
-      <h4>{posts}</h4>
-      <h4>{함수()}</h4>
+      <div className="list">
+        <h3>{ 글제목[0] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[1] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[2] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
