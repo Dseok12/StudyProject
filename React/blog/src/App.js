4b13c75c601f61f278ed91b56980cd24a7a8c3e3
D.seok <ehskstkrjsa@naver.com>
2021-12-04 20:39:50 +0900

    react

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import './App.css';
 import logo from './logo.svg';
@@ -6,7 +7,10 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '우동 맛집', '돈까스 맛집']);
 
+  let [따봉, 따봉변경] = useState(0);
+
   let posts = '강남 고기 맛집';
+
 
 
 
@@ -16,18 +20,18 @@ function App() {
         <div>개발 blog</div>
       </div>
       <div className="list">
-        <h3>{ 글제목[0] }</h3>
+        <h3>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>🎈</span> {따봉} </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
       <div className="list">
         <h3>{ 글제목[1] }</h3>
-        <p>2월 17일 발행</p>
+        <p>2월 18일 발행</p>
         <hr/>
       </div>
       <div className="list">
         <h3>{ 글제목[2] }</h3>
-        <p>2월 17일 발행</p>
+        <p>2월 19일 발행</p>
         <hr/>
       </div>
     </div>
