ae834013a8656931b5d14748c9a5da84e583715d
D.seok <ehskstkrjsa@naver.com>
2021-12-09 22:06:37 +0900

    DeepDive -> DOM Handler

---
+++ JS연습/deepdive/Step4_DOM_Handle/index.js
@@ -0,0 +1,9 @@
+let tags = ["template literals", "javascript", "es6", "밥먹기"];
+
+for (const x of tags) {
+  console.log(x);
+}
+
+// const x = "template literals";
+// const x = "javascript";
+// const x = "es6";
