fff695f4c427e167ef3b5d20c1238d46072f1dc9
D.seok <ehskstkrjsa@naver.com>
2021-12-12 15:29:06 +0900

    프로그래머스 -> 수박수박수 문제 복습

--- JS연습/프로그래머스/01/index.js
+++ JS연습/프로그래머스/01/index.js
@@ -13,10 +13,10 @@ function solution(n) {
   var answer = '';
   // const arr = ["수","박"];
   if(n%2 == 0){
-      // for(let i = 0; i < n; i++){
-      //   "수박".repeat(n)
-      // }
-      answer += "박"
+    // for(let i = 0; i < n; i++){
+    //   "수박".repeat(n)
+    // }
+    answer += "박"
   } else {
     // for(let i = 0; i < n; i++){
     //   "수박".repeat(n)
