e423dd180a6febd43a69897a9dcf943f3ef94eca
D.seok <ehskstkrjsa@naver.com>
2021-12-14 20:24:06 +0900

    DeepDive -> 클로저 개념 이해하기

--- JS연습/deepdive/Step6/index.js
+++ JS연습/deepdive/Step6/index.js
@@ -11,6 +11,7 @@ const innerFunc = outer();
 innerFunc();
 */
 
+/*
 var gn = '홍길동'
 function makePrinter() {
   var on = '김철수'
@@ -24,10 +25,30 @@ function makePrinter() {
 }
 var print = makePrinter()
 print()
-
+*/
 /*
   렉시컬 환경 -> 스코프와 식별자를 관리하는 곳.
   렉시컬 환경은 식별자와 식별자에 바인딩된 값, 그리고 상위 스코프에 대한 참조를 기록하는 자료구조로
   실행 컨텍스트를 구성하는 컴포넌트다. 실행 컨텍스트 스택이 코드의 실행 순서를 관리한다면
   렉시컬 환경은 스코프와 식별자를 관리한다.
 */
+
+
+/* 크리스마스 트리 만들기 */
+
+/*
+const a = 5;
+for(let i = 1; i <= a; i++){
+  let b = '';
+  for(let k = 1; k <= a-i; k++){
+    b += ' ';
+  }
+  for(let j = 1; j <= i*2-1; j++){
+    b += '*';
+  }
+  console.log(b)
+}
+*/
+
+
+
