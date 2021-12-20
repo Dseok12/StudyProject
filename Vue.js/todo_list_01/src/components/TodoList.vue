4941bbb7d03bb6a23781ce91709c767971802178
D.seok <ehskstkrjsa@naver.com>
2021-12-20 15:38:22 +0900

    Vue -> To Do List

--- Vue.js/todo_list_01/src/components/TodoList.vue
+++ Vue.js/todo_list_01/src/components/TodoList.vue
@@ -1,15 +1,36 @@
 <template>
   <div class='TodoList'>
-    Hello List
+    <ul>
+      <li v-for="todoItem in todoItems" :key="todoItem">
+        {{todoItem}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TodoList'
+  name: 'TodoList',
+  data () {
+    return {
+      todoItems: []
+    }
+  },
+  created () {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          this.todoItems.push(localStorage.key(i))
+        }
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+li{
+  list-style: none;
+  padding: 10px 0;
+}
 </style>
