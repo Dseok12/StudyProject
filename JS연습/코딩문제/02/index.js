c9e760c23ac89f6a2a889ef18d551317904d2edb
D.seok <ehskstkrjsa@naver.com>
2021-12-22 22:55:29 +0900

    코딩문제 -> 몫과 나머지

--- JS연습/코딩문제/02/index.js
+++ JS연습/코딩문제/02/index.js
@@ -23,7 +23,7 @@ const squared = (a, b) => {
 squared(2,8)
 */
 
-
+/*
 // a를 b번만큼 반복해서 곱해라
 
 const squared = (a, b) => {
@@ -32,6 +32,27 @@ const squared = (a, b) => {
   }
 }
 squared(2,5)
+*/
+
+
+// -----------------------------------------------------------------
+/*
+몫과 나머지
+공백으로 구분하여 두 숫자가 주어집니다.
+두번째 숫자로 첫번쨰 숫자를 나누었을 때 그 몫과 나머지를 공백으로 구분하여 출력하세요.
+
+입력 : 10 2
+출력 : 5 0
+*/
+
+const num = ['7', '2']
+
+let a = Math.floor(parseInt(num[0], 10)/parseInt(num[1], 10));
+let b = parseInt(num[0], 10)%parseInt(num[1], 10);
+
+console.log(a, b);
+
+
 
 
 
