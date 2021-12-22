c3e15f9bcdeec184836d64a55e7c5246f43cd3ea
D.seok <ehskstkrjsa@naver.com>
2021-12-22 18:21:09 +0900

    코딩문제 -> 제곱구하기

--- JS연습/코딩문제/02/index.js
+++ JS연습/코딩문제/02/index.js
@@ -3,10 +3,37 @@
 
 // a^5 = a * a * a * a * a
 
+/*
+// 내장함수를 사용한 방식
+
 const squared = (a, b) => {
   console.log(Math.pow(a,b))
 }
 squared(2,8);
+*/
+
+
+
+/*
+// 거듭제곱 연산자를 활용한 방식
+
+const squared = (a, b) => {
+  console.log(a**b)
+}
+squared(2,8)
+*/
+
+
+// a를 b번만큼 반복해서 곱해라
+
+const squared = (a, b) => {
+  for(let i = 0; i <= b; i++){
+    console.log(a*(b*i))
+  }
+}
+squared(2,5)
+
+
 
 
 
