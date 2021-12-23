f63edcf5328d49b423f11cdb04c72f7eec04cf54
D.seok <ehskstkrjsa@naver.com>
2021-12-23 17:41:14 +0900

    코딩문제 -> 25번문제까지

--- JS연습/코딩문제/02/index.js
+++ JS연습/코딩문제/02/index.js
@@ -43,7 +43,7 @@ squared(2,5)
 
 입력 : 10 2
 출력 : 5 0
-*/
+
 
 const num = ['7', '2']
 
@@ -51,6 +51,90 @@ let a = Math.floor(parseInt(num[0], 10)/parseInt(num[1], 10));
 let b = parseInt(num[0], 10)%parseInt(num[1], 10);
 
 console.log(a, b);
+*/
+
+
+
+/*
+// set을 만드는 방법 
+var x = new Set('java');
+console.log(x)
+*/
+
+
+
+/*
+// 배수인지 확인하기
+i % 6 == 0;
+
+*/
+
+
+
+/*
+// 23번 O
+*/
+
+
+/*
+대문자로 바꾸기
+입력 : mary
+출력 : MARY
+
+
+const upper = (index) => {
+  let x = index;
+  console.log(x.toUpperCase());
+}
+upper('mary')
+*/
+
+
+/*
+원의 넓이 구하기
+
+const cir = (n) => {
+  console.log(n*n*3.14);
+}
+cir(2)
+*/
+
+
+/*
+26번 행성문제2.
+수성, 금성, 지구, 화성, 목성, 토성, 천왕성, 해왕성
+Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+행성의 한글 이름을 입력하면 영어 이름을 반환하는 프로그램을 만드시오.
+*/
+
+const kor = ['수성', '금성', '지구', '화성', '목성', '토성', '천왕성', '해왕성']
+const eng = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+kor[i] = eng[i];
+
+const result = () => {
+  
+}
+
+
+/*
+27번. 객체만들기
+첫번째 입력에서는 학생의 이름이 공백으로 구분되어 입력되고, 두번째에는 그 학생의 수학 점수가 고백으로 구분되어 주어집니다.
+
+두 개를 합쳐 학생의 이름이 key이고 value가 수학 점수인 객체를 출력하시오.
+
+입력 : Yujin Hyewon
+      70 100
+
+출력 : {'Yujin': 70, 'Hyewon': 100}
+*/
+
+
+
+
+
+
+
 
 
 
