36088925a440e02dd04e7b7e380a697070cffc8a
D.seok <ehskstkrjsa@naver.com>
2021-12-23 21:36:01 +0900

    DeepDive -> 배열

---
+++ JS연습/deepdive/Step9/index.js
@@ -0,0 +1,40 @@
+// 배열 고차함수 reduce로 평균값, 최대값, 요소의 중복 횟수 구하기
+
+/*
+// 평균구하기
+const val1 = [1,2,3,4,5,6];
+
+const average = val1.reduce((acc, cur, i, { length }) => {
+  // 마지막 순회가 아니면 누적값을 반환하고 마지막 순회면 누적값으로 평균을 구해 반환한다.
+  return i === length - 1 ? (acc + cur) / length : acc + cur;
+})
+console.log(average)
+
+
+// 최대값 구하기
+const val2 = [1,2,3,4,5];
+const max = val2.reduce((acc, cur) => (acc > cur ? acc : cur), 0)
+console.log(max)
+*/
+
+
+
+let arr = [200, 100, 300];
+
+const solution = (a, i) => {
+  let a = new Array;
+  let i = 0;
+  
+}
+console.log(solution(1))
+
+
+
+
+
+
+
+
+
+
+
