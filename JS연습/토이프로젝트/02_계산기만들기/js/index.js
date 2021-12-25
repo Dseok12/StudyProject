8048b58643024b496d2099d3851851d77db41bb4
D.seok <ehskstkrjsa@naver.com>
2021-12-25 23:00:14 +0900

    토이프로젝트 -> 계산기만들어보기

--- JS연습/토이프로젝트/02_계산기만들기/js/index.js
+++ JS연습/토이프로젝트/02_계산기만들기/js/index.js
@@ -1,7 +1,31 @@
+const _result = document.getElementById('result'),
+      _num = document.querySelector('.number'),
+      _oper = document.querySelector('.operator'),
+      _ac = document.querySelector('.ac');
 
 
 
 
+const _reset = () => {
+  _ac.addEventListener('click', () => {
+    _result.innerHTML = 0
+  })
+}
+
+const _input = (a) => {
+  _result.innerHTML += a;
+}
+
+const _eval = () => {
+  
+}
+
+
+function init () {
+  _reset();
+  _input();
+}
+init();
 
 
 
