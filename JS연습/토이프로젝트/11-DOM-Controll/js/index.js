b7d79344fd56c8fb141c0f0db074cd12859e4083
D.seok <ehskstkrjsa@naver.com>
2021-12-26 19:27:47 +0900

    토이프로젝트 -> DOMControl

--- JS연습/토이프로젝트/11-DOM-Controll/js/index.js
+++ JS연습/토이프로젝트/11-DOM-Controll/js/index.js
@@ -1,5 +1,22 @@
 
+function close () {
+  const popUp = document.querySelector('.pop');
+  popUp.style.transition = 'all 0.3s'
+  popUp.style.display = 'none';
+}
 
+
+function bind () {
+  const closeBtn = document.querySelector('#close_btn');
+  closeBtn.addEventListener('click', close);
+}
+
+
+function init () {
+  bind();
+}
+
+document.addEventListener('DOMContentLoaded', init);
 
 
 
