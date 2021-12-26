e575a7d44f45fbb50b3ddfeb42ec9a8cfcda349d
D.seok <ehskstkrjsa@naver.com>
2021-12-26 18:22:21 +0900

    코딩문데 -> 28번셋팅

--- JS연습/코딩문제/03/index.js
+++ JS연습/코딩문제/03/index.js
@@ -64,7 +64,24 @@ const 학생2 = {
 console.log(학생2)
 
 
+/*
+28번 2-gram문제
+2-gram이란 문자열에서 2개의 연속된 요소를 출력하는 방법입니다.
+예를 들어 'Jacascript'를 2-gram으로 반복해 본다면 다음과 같은 결과가 나옵니다.
 
+입력: Javascript
+출력
+J a
+a v
+v s
+s c
+c r
+r i
+i p
+p t
+
+입력으로 문자열이 주어지면 2-gram으로 출력하는 프로그램을 작성하시오.
+*/
 
 
 
