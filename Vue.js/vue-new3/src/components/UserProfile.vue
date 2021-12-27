3961018ca98166294e22404305941e7e11ce8bd6
D.seok <ehskstkrjsa@naver.com>
2021-12-27 17:18:22 +0900

    Vue -> Hacker News

---
+++ Vue.js/vue-new3/src/components/UserProfile.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class='userprofile'>
+    <div class="wrap">
+      <div class="inner">
+        <div class="user_container">
+            <div class="user">
+              프로필
+            </div>
+            <div class="link_box">
+              <p>{{ userInfo.id }}</p>
+              <!-- <router-link :to="`/user/${userInfo.user}`" class="user_desc">
+                {{ userInfo.id }}
+              </router-link> -->
+              <div class="time">
+                {{ userInfo.created }}
+              </div>
+            </div>
+            <!-- 제목 -->
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'userprofile',
+  computed: {
+    userInfo () {
+      return this.$store.state.user
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.user_container{
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+  .user{
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin-right: 15px;
+  }
+  .link_box{
+    margin-right: 15px;
+    .user_desc{
+      display: block;
+      font-weight: 600;
+      color: #414141;
+      margin-bottom: 10px;
+    }
+    .time{
+      font-size: 12px;
+    }
+  }
+}
+.title{
+  font-size: 1.4rem;
+  font-weight: 900;
+  margin-bottom: 20px;
+}
+</style>
--- Vue.js/vue-new3/src/views/UserView.vue
+++ Vue.js/vue-new3/src/views/UserView.vue
@@ -2,22 +2,28 @@
   <div class='user'>
     <div class="wrap">
       <div class="inner">
-        <p>사용자 이름: {{userInfo.id}}</p>
+        <user-profile></user-profile>
+        <!-- <p>사용자 이름: {{userInfo.id}}</p>
         <p>활동 점수: {{userInfo.karma}}</p>
-        <p>아이디 생성일: {{userInfo.created}}</p>
+        <p>아이디 생성일: {{userInfo.created}}</p> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue'
+
 export default {
   name: 'user',
-  computed: {
-    userInfo () {
-      return this.$store.state.user
-    }
+  components: {
+    UserProfile
   },
+  // computed: {
+  //   userInfo () {
+  //     return this.$store.state.user
+  //   }
+  // },
   created () {
     const userName = this.$route.params.id
     this.$store.dispatch('FETCH_USER', userName)
