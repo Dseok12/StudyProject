df834d7ca701417e919fff959dd74334063c1106
D.seok <ehskstkrjsa@naver.com>
2021-12-27 17:18:51 +0900

    토이프로젝트 -> DOM Control

--- JS연습/토이프로젝트/11-DOM-Controll/js/index.js
+++ JS연습/토이프로젝트/11-DOM-Controll/js/index.js
@@ -1,4 +1,27 @@
 
+
+function hor () {
+
+}
+
+
+function hover () {
+  const subMenu = document.querySelector('.submenu')
+  if(addEventListener('mouseover') == true){
+    subMenu.className += 'block'
+  }else{
+    subMenu.className -= 'block'
+  }
+}
+
+function hoverBind () {
+  const mainMeun = document.querySelector('.menu li');
+  mainMeun.addEventListener('mouseover', hover)
+}
+
+
+
+// 팝업 닫기
 function close () {
   const popUp = document.querySelector('.pop');
   popUp.style.transition = 'all 0.3s'
@@ -12,11 +35,15 @@ function bind () {
 }
 
 
+// 함수 실행기
 function init () {
+  document.addEventListener('DOMContentLoaded',() => {});
   bind();
+  hoverBind();
 }
+init();
 
-document.addEventListener('DOMContentLoaded', init);
+
 
 
 
