9e72fa8c9b4d0f2e986176dfbad80d742fea83da
D.seok <ehskstkrjsa@naver.com>
2022-01-01 23:00:49 +0900

    토이프로젝트 -> 바닐라자바스크립트로 To Do List 만들기

--- JS연습/토이프로젝트/03/js/index.js
+++ JS연습/토이프로젝트/03/js/index.js
@@ -1,5 +1,32 @@
 
-
+const createTodoElement = () => {
+  const _todoItem = document.createElement('div');
+  _todoItem.innerHTML = `
+          <div class="content">
+            <input type="checkbox" class="todo_checkbox">
+            <label></label>
+            <input type="text" value="" class="todo_content">
+          </div>
+          <div class="btn_wrap">
+            <div class="item_buttons content_buttons">
+              <button class="todo_edit_button btn">
+                수정
+              </button>
+              <button class="todo_remove_button btn">
+                삭제
+              </button>
+            </div>
+            <div class="item_buttons edit_buttons">
+              <button class="todo_edit_confirm_button btn">
+                수정확인
+              </button>
+              <button class="todo_edit_cancel_button btn">
+                취소
+              </button>
+            </div>
+          </div>
+  `
+}
 
 
 
