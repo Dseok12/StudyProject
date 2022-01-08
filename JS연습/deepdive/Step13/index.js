bd6eda9040f852a13b80b9d2c34e46c818e693fa
D.seok <ehskstkrjsa@naver.com>
2022-01-08 22:16:47 +0900

    for ... of 문 사용법

---
+++ JS연습/deepdive/Step13/index.js
@@ -0,0 +1,13 @@
+const str = '안녕하세요'
+const str1 = [1,2,3]
+
+for (const val of str) {
+  console.log(val)
+}
+
+for (const val of str1) {
+  console.log(val)
+}
+
+let str2 = 1;
+console.log(typeof String(str2))
