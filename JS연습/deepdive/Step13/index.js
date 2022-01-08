908ab8928bfc2439270d0ab8a534a4f7c0865349
D.seok <ehskstkrjsa@naver.com>
2022-01-08 22:19:54 +0900

    for ... of문 정리

--- JS연습/deepdive/Step13/index.js
+++ JS연습/deepdive/Step13/index.js
@@ -1,13 +1,9 @@
-const str = '안녕하세요'
-const str1 = [1,2,3]
-
+const str = '안녕하세요';
 for (const val of str) {
   console.log(val)
 }
 
+const str1 = [1,2,3];
 for (const val of str1) {
   console.log(val)
 }
-
-let str2 = 1;
-console.log(typeof String(str2))
