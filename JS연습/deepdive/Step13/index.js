494eaf7caad54af4c182619b363aede339c6833b
D.seok <ehskstkrjsa@naver.com>
2022-01-10 20:55:12 +0900

    DeepDive -> 12장, 13장 예제 연습

--- JS연습/deepdive/Step13/index.js
+++ JS연습/deepdive/Step13/index.js
@@ -1,3 +1,5 @@
+
+/*
 const str = '안녕하세요';
 for (const val of str) {
   console.log(val)
@@ -7,3 +9,57 @@ const str1 = [1,2,3];
 for (const val of str1) {
   console.log(val)
 }
+*/
+
+
+/*
+// 재귀함수
+function countdown (n) {
+  if( n < 0 ) return;
+  console.log(n);
+  countdown( n - 1 )
+}
+countdown (10)
+// 재귀함수 -> n!
+function factorial (n) {
+  // 탈출 조건: n이 1 이하일 때 재귀 호출을 멈춘다.
+  if( n <= 1 ) return 1;
+
+  return n * factorial( n - 1 )
+}
+
+console.log(factorial(0))
+console.log(factorial(1))
+console.log(factorial(2))
+console.log(factorial(3))
+*/
+
+/*
+// repeat함수 만들기
+function repeat1 (n) {
+  for(let i = 0; i <= n; i++){console.log(i)}
+}
+repeat1(3)
+*/
+
+/*
+var x = 1;
+
+function foo () {
+  var x = 10;
+  bar();
+}
+function bar () {
+  console.log(x)
+}
+foo()
+bar()
+*/
+
+
+
+
+
+
+
+
