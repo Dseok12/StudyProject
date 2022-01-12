56b8031c467cec8571fe2c4d14f74cae7bf50446
D.seok <ehskstkrjsa@naver.com>
2022-01-12 19:32:55 +0900

    Test Code -> Test Code 실행 Test

--- JS연습/TDD/index.js
+++ JS연습/TDD/index.js
@@ -1,4 +1,6 @@
 /* eslint-disable */
-const 소수찾기 = () => {};
+const 소수찾기 = (_input) => {
+  if(_input <= 1) {return `입력하신 값은 ${_input}이므로 소수가 될 수 없습니다.`}
+};
 
 export default 소수찾기;
--- JS연습/TDD/test/index.test.js
+++ JS연습/TDD/test/index.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-const sum = (a, b) => a + b;
-test("5 + 10 = 15", () => {
-  expect(sum(5, 10)).toBe(15);
+import 소수찾기 from '../index.js'
+test("1이하의 소수는 몇개?", () => {
+  expect(소수찾기(1)).toEqual(`입력하신 값은 1이므로 소수가 될 수 없습니다.`);
 });
