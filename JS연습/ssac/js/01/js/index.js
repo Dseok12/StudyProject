95db83fa8d7aec963adb6df03ec712f97bb929b4
D.seok <ehskstkrjsa@naver.com>
2022-01-26 13:48:29 +0900

    ssac

--- JS연습/ssac/js/01/js/index.js
+++ JS연습/ssac/js/01/js/index.js
@@ -1,6 +1,42 @@
+function moviPic1 () {
+  // console.log(document.getElementById('pic').src)
+  document.getElementById('pic').src="./img/movie1.jpg"
+}
+
+function moviPic2 () {
+  document.getElementById('pic').src="./img/movie2.jpg"
+}
+
+function moviPic3 () {
+  document.getElementById('pic').src="./img/movie3.jpg"
+}
 
 
-
+function commentAlign1 () {
+  // console.log(document.querySelector('.text').style.textAlign = 'left')
+  document.getElementById('comment').style.textAlign = 'left'
+  document.getElementById('comment').style.backgroundColor = 'red'
+  document.getElementById('comment').style.color = '#fff'
+  // document.querySelector('.text').style.textAlign = 'left'
+  // document.querySelector('.text').style.backgroundColor = 'red'
+  // document.querySelector('.text').style.color = '#fff'
+}
+function commentAlign2 () {
+  document.getElementById('comment').style.textAlign = 'center'
+  document.getElementById('comment').style.backgroundColor = 'blue'
+  document.getElementById('comment').style.color = 'yellow'
+  // document.querySelector('.text').style.textAlign = 'center'
+  // document.querySelector('.text').style.backgroundColor = 'blue'
+  // document.querySelector('.text').style.color = '#fff'
+}
+function commentAlign3 () {
+  document.getElementById('comment').style.textAlign = 'right'
+  document.getElementById('comment').style.backgroundColor = 'yellow'
+  document.getElementById('comment').style.color = 'black'
+  // document.querySelector('.text').style.textAlign = 'right'
+  // document.querySelector('.text').style.backgroundColor = 'yellow'
+  // document.querySelector('.text').style.color = 'black'
+}
 
 
 
