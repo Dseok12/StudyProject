f932bf8d9782de89ec5806e904742cbecf4a62d1
D.seok <ehskstkrjsa@naver.com>
2022-01-26 14:33:46 +0900

    ssac

--- JS연습/ssac/js/01/js/index.js
+++ JS연습/ssac/js/01/js/index.js
@@ -1,3 +1,4 @@
+/*
 function moviPic1 () {
   // console.log(document.getElementById('pic').src)
   document.getElementById('pic').src="./img/movie1.jpg"
@@ -14,34 +15,42 @@ function moviPic3 () {
 
 function commentAlign1 () {
   // console.log(document.querySelector('.text').style.textAlign = 'left')
-  document.getElementById('comment').style.textAlign = 'left'
+  document.getElementById('comment').align = 'left'
   document.getElementById('comment').style.backgroundColor = 'red'
   document.getElementById('comment').style.color = '#fff'
-  // document.querySelector('.text').style.textAlign = 'left'
+  // document.querySelector('.text').align = 'left'
   // document.querySelector('.text').style.backgroundColor = 'red'
   // document.querySelector('.text').style.color = '#fff'
 }
 function commentAlign2 () {
-  document.getElementById('comment').style.textAlign = 'center'
+  document.getElementById('comment').align = 'center'
   document.getElementById('comment').style.backgroundColor = 'blue'
   document.getElementById('comment').style.color = 'yellow'
-  // document.querySelector('.text').style.textAlign = 'center'
+  // document.querySelector('.text').align = 'center'
   // document.querySelector('.text').style.backgroundColor = 'blue'
   // document.querySelector('.text').style.color = '#fff'
 }
 function commentAlign3 () {
-  document.getElementById('comment').style.textAlign = 'right'
+  document.getElementById('comment').align = 'right'
   document.getElementById('comment').style.backgroundColor = 'yellow'
   document.getElementById('comment').style.color = 'black'
-  // document.querySelector('.text').style.textAlign = 'right'
+  // document.querySelector('.text').align = 'right'
   // document.querySelector('.text').style.backgroundColor = 'yellow'
   // document.querySelector('.text').style.color = 'black'
 }
+*/
 
 
+function moviPic (num) {
+  // document.getElementById('pic').src=`./img/movie${num}.jpg`;
+  document.getElementById('pic').src='./img/movie' + num + '.jpg';
+}
 
-
-
+function commentAlign (align, background, color) {
+  document.getElementById("comment").align = align
+  document.getElementById("comment").style.background = background
+  document.getElementById('comment').style.color = color
+}
 
 
 
