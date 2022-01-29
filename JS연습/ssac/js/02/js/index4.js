f14f60c4b3871795e51f72106f2f008a899ff3c3
D.seok <ehskstkrjsa@naver.com>
2022-01-29 17:05:55 +0900

    ssac

--- JS연습/ssac/js/02/js/index4.js
+++ JS연습/ssac/js/02/js/index4.js
@@ -1,3 +1,4 @@
+/*
 var bannerWith = 697;
 var isnum = 0;
 
@@ -21,3 +22,67 @@ function rightBtn () {
 function animate (isnum) {
   document.getElementsByClassName('bannerList')[0].style.left = -(bannerWith*isnum) + 'px';
 }
+*/
+
+
+var bannerWidth = 697;
+var isnum = 0;
+
+function leftBtn () {
+  isnum--;
+  if(isnum < 0){
+    isnum = 4;
+  };
+  animate(isnum)
+}
+
+function rightBtn () {
+  isnum++;
+  if(isnum >= 4){
+    isnum = 0;
+  };
+  animate(isnum)
+}
+function animate (isnum) {
+  document.getElementsByClassName('bannerList')[0].style.left = -(bannerWidth*isnum) + 'px';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
