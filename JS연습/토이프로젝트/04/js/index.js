8f3b6cc5e6386fdf7032f99f8e1970dbaf7321a8
D.seok <ehskstkrjsa@naver.com>
2022-01-30 22:22:36 +0900

    토이프로젝트 -> localStorage 연습

--- JS연습/토이프로젝트/04/js/index.js
+++ JS연습/토이프로젝트/04/js/index.js
@@ -1,25 +1,47 @@
+// 헬퍼 함수
+const _get = (target) => {
+  return document.querySelector(target)
+}
 
-var idInput = document.querySelector('.id_input');
-var pwInput = document.querySelector('.pw_input');
-
-var _id = idInput.value;
-var _pw = pwInput.value;
-
-
-function submit (e) {
+function _submit (e) {
   e.preventDefault();
+  e.stopPropagation();
+  
+  var idInput = _get('.id_input');
+  var pwInput = _get('.pw_input');
+  var _id = idInput.value;
+  var _pw = pwInput.value;
+  
+  if(_id === '' || _pw === ''){
+    idInput.focus()
+    alert('아이디 혹은 비밀번호를 입력하세요.')
+    return
+  }
+
+  fetch('http://127.0.0.1:5500/04/index.html')
+    .then(console.log('Hi'))
+    .catch((err) => console.error(err))
+
+}
+
+function getToken () {
+  return localStorage.getItem('token');
+}
+
+function inputInfo () {
   var formBox = document.querySelector('#form_box')
-  // console.log('hi form')
-  formBox.addEventListener('submit', inputInfo)
+  formBox.addEventListener('submit', _submit)
 }
 
 function bindSubmitBtn () {
   var _submitBtn = document.querySelector('.submitBtn')
-  _submitBtn.addEventListener('click', submit)
+  _submitBtn.addEventListener('click', _submit)
 }
 
 function main () {
   bindSubmitBtn()
+
+  var token = getToken();
 }
 
 window.addEventListener('DOMContentLoaded', main)
