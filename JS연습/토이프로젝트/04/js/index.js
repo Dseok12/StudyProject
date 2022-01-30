46b3cd689d680d89a4bb19cdb41b8723a3683513
D.seok <ehskstkrjsa@naver.com>
2022-01-30 21:51:02 +0900

    토이프로젝트 -> localStorage 연습

--- JS연습/토이프로젝트/04/js/index.js
+++ JS연습/토이프로젝트/04/js/index.js
@@ -1,4 +1,28 @@
 
+var idInput = document.querySelector('.id_input');
+var pwInput = document.querySelector('.pw_input');
+
+var _id = idInput.value;
+var _pw = pwInput.value;
+
+
+function submit (e) {
+  e.preventDefault();
+  var formBox = document.querySelector('#form_box')
+  // console.log('hi form')
+  formBox.addEventListener('submit', inputInfo)
+}
+
+function bindSubmitBtn () {
+  var _submitBtn = document.querySelector('.submitBtn')
+  _submitBtn.addEventListener('click', submit)
+}
+
+function main () {
+  bindSubmitBtn()
+}
+
+window.addEventListener('DOMContentLoaded', main)
 
 
 
