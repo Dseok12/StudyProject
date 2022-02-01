101c40a67b542488718f524179bc0dd07336f23e
D.seok <ehskstkrjsa@naver.com>
2022-02-01 22:42:13 +0900

    토이프로젝트 -> localStorage 연습하기02

--- JS연습/토이프로젝트/05_localStorage를 활용한 회원가입기능만들어보기/js/index.js
+++ JS연습/토이프로젝트/05_localStorage를 활용한 회원가입기능만들어보기/js/index.js
@@ -4,31 +4,54 @@ const _get = (target) => {
 }
 
 
+function makeId() {
+  var _id = _get('.id');
+  var idValue = _id.value;
 
+  localStorage.setItem('ID', idValue)
+}
 
+function makeNick() {
+  var _nick = _get('.nickname');
+  var nickValue = _nick.value;
 
+  localStorage.setItem('nickname', nickValue);
+}
 
+function makePw() {
+  var _makePw = _get('.pw01');
+  var _makePwValue = _makePw.value;
 
+  localStorage.setItem('password', _makePwValue);
+}
 
+function comparePw() {
+  var _comparePw = _get('.pw02')
+  var _comparePwValue = _comparePw.value
+  var compare01 = localStorage.getItem('password')
 
+  if(_comparePwValue !== compare01){
+    alert('비밀번호가 다릅니다!!!!')
+  }
+}
 
+function submitForm() {
+  makeId();
+  makeNick();
+  makePw();
+  comparePw();
+}
 
+function submitBtn(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  var _submit = _get('.submitBtn')
+  
+  _submit.addEventListener('click', submitForm)
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-function main () {
-
+function main() {
+  submitBtn()
 }
 
 window.addEventListener('DOMContentLoaded', main)
