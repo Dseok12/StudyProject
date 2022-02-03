fb4438a15e64dd164fa3ba750dafb1d26ba31815
D.seok <ehskstkrjsa@naver.com>
2022-02-03 12:58:18 +0900

    토이프로젝트 -> localStorage 연습 (로그인 validation check) 절반 성공

--- JS연습/토이프로젝트/06_localStorage연습03/js/index.js
+++ JS연습/토이프로젝트/06_localStorage연습03/js/index.js
@@ -1,23 +1,71 @@
 var user = {
-  id: 'come',
+  id: 'qwe',
   pw: 123
 }
 
 var _setLocal = localStorage.setItem('user', JSON.stringify(user))
 var _getLocal = JSON.parse(localStorage.getItem('user'))
+// var strGetLocal = JSON.stringify(_getLocal)
+console.log(_setLocal)
+console.log(_getLocal)
+const _get = (target) => {
+  return document.querySelector(target)
+}
 
+var _idInput = _get('.id_input');
+var _pwInput = _get('.pw_input');
+var _idValue = _idInput.value;
+var _pwValue = Number(_pwInput.value);
 
+console.log(typeof _idValue)
+console.log(typeof _pwValue)
+console.log(typeof _getLocal.id)
+console.log(typeof _getLocal.pw)
+console.log(_getLocal.id)
+console.log(_getLocal.pw)
 
+// localStorage 삭제
+function _removeLocal(e) {
+  e.preventDefault();
+  _get('.wellcome_user').style.display= 'none'
+  _get('.login_btn').style.display = 'block'
+  alert('로그아웃이 되었습니다.')
+  localStorage.removeItem('user')
+}
 
+function _bindRemoveLocal() {
+  var _loginoutBtn = _get('.loginout_btn')
+  
+  _loginoutBtn.addEventListener('click', _removeLocal)
+}
+// //localStorage 삭제
 
+function compare(e) {
+  e.preventDefault();
 
+  if(_idValue == _getLocal.id && _pwValue == _getLocal.pw){
+    alert(`환영합니다!! ${_getLocal.id}님!`)
+    console.log('로그인에 성공하였습니다.')
+    _get('.login_btn').style.display = 'none';
+    _get('.wellcome_user').innerHTML = `환영합니다. ${_getLocal.id}님`;
+  } else {
+    alert('로그인에 실패하였습니다!!')
+    console.error('로그인에 실패하였습니다.')
+  }
 
+}
 
+function _bindSubmitBtn() {
+  var _loginBtn = _get('.login_btn')
+
+  _loginBtn.addEventListener('click', compare)
+}
 
 
 
 const main = () => {
-
+  _bindSubmitBtn()
+  _bindRemoveLocal()
 } 
 
 const init = () => {
