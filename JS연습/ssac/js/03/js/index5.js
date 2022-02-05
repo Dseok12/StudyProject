a13438336967a34cd51419600fae02c2d9c9eeb3
D.seok <ehskstkrjsa@naver.com>
2022-02-05 17:04:49 +0900

    ssac

--- JS연습/ssac/js/03/js/index5.js
+++ JS연습/ssac/js/03/js/index5.js
@@ -1,1 +1,41 @@
-js
+
+function selectMenu(num) {
+  if(currentNum != num) {
+    liList[currentNum].className = '';
+    liList[num].className = 'on';
+    currentNum=num;
+  }
+}
+
+var currentNum = 0;
+var liList;
+
+function init() {
+  liList = document.getElementById('serviceList').getElementsByTagName('li')
+  for(var i = 0; i < liList.length; i++){
+    liList[i].num = i;
+    liList[i].onmouseover = function() {
+      selectMenu(this.num)
+    }
+  }
+}
+
+window.onload = init;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
