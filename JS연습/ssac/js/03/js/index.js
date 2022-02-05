ee6bbd9a25206f8ce15f833d23cc2d7692ba4ac4
D.seok <ehskstkrjsa@naver.com>
2022-02-05 11:12:02 +0900

    ssac

--- JS연습/ssac/js/03/js/index.js
+++ JS연습/ssac/js/03/js/index.js
@@ -20,7 +20,31 @@ function interparkradiobtn2() {
   document.getElementById('interparradio_1').checked=false;
 }
 
+/*
+window.onload = function () {
+  var interpark1 = document.getElementById('sel1')
+  var interpark2 = document.getElementById('sel2')
+  var btn1 = document.getElementById('interparradio_1')
+  var btn2 = document.getElementById('interparradio_2')
 
+  btn1.onclick = function () {
+    if(interpark1.clasName == 'off'){
+      interpark1.clasName = 'on'
+      document.getElementById('interparradio_1').checked=true;
+    }
+    interpark2.clasName = 'off'
+    document.getElementById('interparradio_2').checked=false;
+  }
+  btn2.onclick = function () {
+    if(interpark2.clasName == 'off'){
+      interpark2.clasName = 'on'
+      document.getElementById('interparradio_2').checked=true;
+    }
+    interpark1.clasName = 'off'
+    document.getElementById('interparradio_1').checked=false;
+  }
+}
+*/
 
 
 
