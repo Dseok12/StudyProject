79ab59a7621dd91e821ff081387ad3931686ce20
D.seok <ehskstkrjsa@naver.com>
2022-02-05 09:58:48 +0900

    ssac

--- JS연습/ssac/js/03/js/index.js
+++ JS연습/ssac/js/03/js/index.js
@@ -1,8 +1,20 @@
 
 
+var interpark1 = document.getElementById('sel1')
+var interpark2 = document.getElementById('sel2')
 
-
-
+function interparkradiobtn1() {
+  if(interpark1.clasName == 'off'){
+    interpark1.clasName = 'on'
+  }
+  interpark2.clasName = 'off'
+}
+function interparkradiobtn2() {
+  if(interpark2.clasName == 'off'){
+    interpark2.clasName = 'on'
+  }
+  interpark1.clasName = 'off'
+}
 
 
 
