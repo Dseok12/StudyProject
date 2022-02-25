73bb09b47341882701ad583b23c71b8bd965620f
D.seok <ehskstkrjsa@naver.com>
2022-02-25 18:39:11 +0900

    forEach, mpa, filter, reduce 연습

--- JS연습/코딩문제/08/index.js
+++ JS연습/코딩문제/08/index.js
@@ -1,13 +1,29 @@
 a = [10, 11, 12, 13, 14, 15]
 
-a.forEach((v, hi) => {
-  console.log(v, hi)
-})
+a.forEach(function(v, i){
+  console.log(v, i, this)
+}, [1, 2])
 
 
+// map -> 모든 배열을 전부 꺼내고 조건이 만족하지 않을 때에는 undefined로 출력된다.
+let answer = a.map(function(v, i){
+  return v*i
+}, [1, 2])
+console.log(answer)
 
 
+// filter 딱 원하는 값만 뽑아내서 새로운 배열을 만든다.
+let answer2 = a.filter(function(v, i){
+  return v%2 == 1;
+}, [1, 2])
+console.log(answer2)
 
+
+// reduce는 배열을 출력하는 것이 아니라 값을 출력한다.
+let answer3 = a.reduce(function(acc, v){
+  return acc + v;
+}, 0)
+console.log(answer3)
 
 
 
