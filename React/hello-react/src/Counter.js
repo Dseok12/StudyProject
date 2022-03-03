f4601f00513e83aabef92ea4bc9b709defab5853
D.seok <ehskstkrjsa@naver.com>
2022-03-03 23:43:46 +0900

    React -> restart

--- React/hello-react/src/Counter.js
+++ React/hello-react/src/Counter.js
@@ -37,6 +37,16 @@ class Counter extends Component {
         >
           -1
         </button>
+        <button
+          onClick={() => {
+            this.setState({number: number+1}, () => {
+              console.log('방금 setState가 호출되었습니다.')
+              console.log(this.state)
+            })
+          }}
+        >
+          함수호출 버튼
+        </button>
       </div>
     );
   }
