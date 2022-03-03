8f1d58a1e86fe0ed23d4a3a5916fd68c8c9457c6
D.seok <ehskstkrjsa@naver.com>
2022-03-03 17:35:01 +0900

    PortFolio -> 오류 해결

--- PortFolio(publishing)/MY/js/index_jquery.js
+++ PortFolio(publishing)/MY/js/index_jquery.js
@@ -5,7 +5,22 @@ $(document).ready(function(){
     $('.m_menu_box').fadeToggle(300)
   })
 
+  // Contact Tab Menu
+  var _tabAnchor = $('.tab_btn li'),
+      _contactBox = $('.contact_box');
 
+      _tabAnchor.click(function(e){
+        e.preventDefault();
+
+        $(this).find('a').addClass('active');
+        $(this).siblings().find('a').removeClass('active');
+
+        _contactBox.hide();
+
+        var _targetIdx = $(this).index();
+        _contactBox.eq(_targetIdx).show();
+      });
+      _tabAnchor.eq(0).trigger('click');
 
 
 
