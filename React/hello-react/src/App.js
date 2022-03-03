72812044bc70b57f44c4086d0a7fd6c9c3b0da87
D.seok <ehskstkrjsa@naver.com>
2022-03-03 15:33:08 +0900

    React -> restart

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = `리액트`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {name === `리액트` ? (
+        <h1>리액트입니다.</h1>
+      ) : (
+        <h2>리액트가 아닙니다</h2>
+      )}
     </div>
   );
 }
