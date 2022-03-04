4eff9994f315a8c92d5ba1911ae5996c9161f249
D.seok <ehskstkrjsa@naver.com>
2022-03-04 20:53:35 +0900

    PortFolio -> Update

--- PortFolio(publishing)/MY/js/index_jquery.js
+++ PortFolio(publishing)/MY/js/index_jquery.js
@@ -4,23 +4,9 @@ $(document).ready(function(){
     $('.m_btn').toggleClass('on');
     $('.m_menu_box').fadeToggle(300)
   })
+  /* //모바일버튼 부분 */
 
-  // Contact Tab Menu
-  var _tabAnchor = $('.tab_btn li'),
-      _contactBox = $('.contact_box');
 
-      _tabAnchor.click(function(e){
-        e.preventDefault();
-
-        $(this).find('a').addClass('active');
-        $(this).siblings().find('a').removeClass('active');
-
-        _contactBox.hide();
-
-        var _targetIdx = $(this).index();
-        _contactBox.eq(_targetIdx).show();
-      });
-      _tabAnchor.eq(0).trigger('click');
 
 
 
