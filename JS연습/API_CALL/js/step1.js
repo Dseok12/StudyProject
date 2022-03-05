eef510731692a7de4ad3d7ed7670676e4eba763c
D.seok <ehskstkrjsa@naver.com>
2022-03-05 19:14:49 +0900

    JS -> API_CALL

--- JS연습/API_CALL/js/step1.js
+++ JS연습/API_CALL/js/step1.js
@@ -1,18 +1,22 @@
-const API = `https://api.instantwebtools.net/v1/airlines`
+const API = `https://api.instantwebtools.net/v1/airlines`;
+
 const _get = (target) => {
-  return document.querySelector(target)
+  return document.querySelector(target);
 }
+
 const handleLoadData = async () => {
   await fetch(API)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data)
-      data.forEach((m) => {
-      _get('.name').innerHTML = m.name
-      _get('.country').innerHTML = m.country
-      // console.log(m)
-      })
+  .then((res) => res.json())
+  .then((data) => {
+    // console.table(data)
+    for( var i = 0; i <= 10; i++){
+      // document.createElement('p') = data[i].name
+      // console.log(data[i].name)
+      document.write('<p class="name">' + data[i].name + '</p>')
+      // document.createElement('p').innerHTML = data[i].name
+    }
+      // _get('.website').innerHTML = data[0].website
     })
     .catch((err) => console.error(err))
 }
-handleLoadData()
+handleLoadData();
