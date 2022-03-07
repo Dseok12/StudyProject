df9b19b04df4ea177386ac9d35ecf04dcdb4cb04
D.seok <ehskstkrjsa@naver.com>
2022-03-07 17:04:53 +0900

    JS -> API_CALL 정리

--- JS연습/API_CALL/js/step1.js
+++ JS연습/API_CALL/js/step1.js
@@ -10,12 +10,8 @@ const handleLoadData = async () => {
   .then((data) => {
     // console.table(data)
     for( var i = 0; i <= 10; i++){
-      // document.createElement('p') = data[i].name
-      // console.log(data[i].name)
       document.write('<p class="name">' + data[i].name + '</p>')
-      // document.createElement('p').innerHTML = data[i].name
     }
-      // _get('.website').innerHTML = data[0].website
     })
     .catch((err) => console.error(err))
 }
