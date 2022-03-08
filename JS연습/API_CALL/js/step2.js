fe165bf46250197f78824c60aad7f40f1de25235
D.seok <ehskstkrjsa@naver.com>
2022-03-08 19:06:30 +0900

    JS연습 -> API_POST

---
+++ JS연습/API_CALL/js/step2.js
@@ -0,0 +1,30 @@
+const API = `https://api.instantwebtools.net/v1/airlines`;
+
+const _get = (target) => {
+  return document.querySelector(target);
+}
+
+
+const handleLoadData = async (e) => {
+  e.preventDefault();
+  const address = {
+    name,
+    country
+  } 
+  await fetch(API, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify()
+  })
+  .then((res) => res.json())
+  .then(() => {
+    console.log('데이터 보내기 성공')
+  })
+  .catch((err) => console.error(err))
+}
+// handleLoadData();
+const init = () => {
+  _get('.submit_btn').addEventListener('submit',handleLoadData)
+}
+
+init();
