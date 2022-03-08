84a55f359ede4c90a7e4183fe0600ba066fc3de0
D.seok <ehskstkrjsa@naver.com>
2022-03-08 18:34:11 +0900

    UX/UI - 연습 -> scroll Event opacity

--- JS연습/UX_UI - 연습/01/js/index.js
+++ JS연습/UX_UI - 연습/01/js/index.js
@@ -1,4 +1,27 @@
+$(document).ready(function(){
+  var threshold02 = $('.section02').offset().top-300;
+  var threshold03 = $('.section03').offset().top-300;
+  // var scrollPOS = $(window).scrollTop();
 
+  $(window).scroll(function(){
+    // console.log(threshold02)
+    if($(window).scrollTop() >= threshold02){
+      $('.section02 .text_box').removeClass('off')
+      $('.section02 .text_box').addClass('on')
+    }else{
+      $('.section02 .text_box').addClass('off')
+      $('.section02 .text_box').removeClass('on')
+    }
+
+    if($(window).scrollTop() >= threshold03){
+      $('.section03 .text_box').removeClass('off')
+      $('.section03 .text_box').addClass('on')
+    }else{
+      $('.section03 .text_box').addClass('off')
+      $('.section03 .text_box').removeClass('on')
+    }
+  })
+})
 
 
 
