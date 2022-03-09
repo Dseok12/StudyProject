0ce92b5dc35cbeef683fc4f530a0366f49c8de59
D.seok <ehskstkrjsa@naver.com>
2022-03-09 21:33:14 +0900

    React - restart NewsApi

---
+++ React/news-viewer/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import NewsList from "./components/NewsList";
+
+function App() {
+  return <NewsList />
+}
+
+export default App;
---
+++ React/news-viewer/src/components/NewsList.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from 'styled-components';
+import NewsItem from "./NewsItem";
+
+const NewsListBlock = styled.div`
+box-sizing: border-box;
+padding-bottom: 3rem;
+width: 768px;
+margin: 0 auto;
+margin-top: 2rem;
+
+@media screen and (max-width:768px){
+  width: 100%;
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+`;
+
+const smapleArticel = {
+  title: '제목',
+  description: '내용',
+  url: 'https://google.com',
+  urlToImage: 'https://via.placeholder.com/160'
+}
+
+const NewsList = () => {
+  return (
+    <NewsListBlock>
+      <NewsItem article={smapleArticel} />
+      <NewsItem article={smapleArticel} />
+      <NewsItem article={smapleArticel} />
+      <NewsItem article={smapleArticel} />
+      <NewsItem article={smapleArticel} />
+      <NewsItem article={smapleArticel} />
+    </NewsListBlock>
+  )
+}
+
+
+
+
+
+export default NewsList;
