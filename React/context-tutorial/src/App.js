300839e7c657accf91db78b2ff7a056112e5a390
D.seok <ehskstkrjsa@naver.com>
2022-03-12 17:20:24 +0900

    React - ContextAPI

--- React/context-tutorial/src/App.js
+++ React/context-tutorial/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ColorBox from "./components/ColorBox";
-import ColorContext from "./contexts/color";
+import { ColorProvider } from "./contexts/color";
 
-function App() {
+const App = () => {
   return (
-    <ColorContext.Provider value={{ color: 'red' }}>
+    <ColorProvider>
       <div>
         <ColorBox />
       </div>
-    </ColorContext.Provider>
+    </ColorProvider>
   );
 }
 
