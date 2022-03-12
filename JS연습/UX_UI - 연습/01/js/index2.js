3f355c8ae415d490dcb446ef4e956fd27a846c3b
D.seok <ehskstkrjsa@naver.com>
2022-03-12 17:20:47 +0900

    UX/UI -> canvas 연습

---
+++ JS연습/UX_UI - 연습/01/js/index2.js
@@ -0,0 +1,22 @@
+// const canvas = document.querySelector('canvas')
+// if(canvas.getContext){
+//   console.log('캔버스 지원')
+// } else {
+//   console.log('캔버스 미지원')
+// }
+
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+console.log(context)
+
+
+
+
+
+
+
+
+
+
+
+
