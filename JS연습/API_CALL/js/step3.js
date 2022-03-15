7a28569278dd60544a591bbb7b70c480fb92f991
D.seok <ehskstkrjsa@naver.com>
2022-03-15 10:33:16 +0900

    JS-> API_CALL Ajax

---
+++ JS연습/API_CALL/js/step3.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+  $.ajax({
+    type: 'GET',
+    url: `https://newsapi.org/v2/top-headlines?country=kr&apiKey=fe3a727c3dd3403890cfad606e65b73d`,
+    // data: ,
+    dataType: JSON,
+    success: function(){
+      console.log(data)
+    },
+    error: function(jqXHR, textStatus, errorThrown){
+      console.log(textStatus+", "+errorThrown)
+    }
+  })
+
+
+
+
+
+
+
+
+
+});
