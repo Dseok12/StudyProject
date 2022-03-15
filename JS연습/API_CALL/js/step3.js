9a5758f4c7733099065fb34bfbcea586cad8ed4b
D.seok <ehskstkrjsa@naver.com>
2022-03-15 13:31:33 +0900

    Ajax 연습

--- JS연습/API_CALL/js/step3.js
+++ JS연습/API_CALL/js/step3.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
 
-  $.ajax({
-    type: 'GET',
-    url: `https://newsapi.org/v2/top-headlines?country=kr&apiKey=fe3a727c3dd3403890cfad606e65b73d`,
-    // data: ,
-    dataType: JSON,
-    success: function(){
-      console.log(data)
-    },
-    error: function(jqXHR, textStatus, errorThrown){
-      console.log(textStatus+", "+errorThrown)
-    }
-  })
+  function getList () {
+    $.ajax({
+      type: 'GET',
+      url: `https://newsapi.org/v2/top-headlines?country=kr&apiKey=fe3a727c3dd3403890cfad606e65b73d`,
+      // data: ,
+      dataType: JSON,
+      success: function(){
+        console.log(data)
+      },
+      error: function(jqXHR, textStatus, errorThrown){
+        console.log(textStatus+", "+errorThrown)
+      }
+    })
+  }
 
 
 
