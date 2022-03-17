ebe5433011e37f77370aec8e5987d9a2baa91888
D.seok <ehskstkrjsa@naver.com>
2022-03-17 16:48:56 +0900

    JS -> POST 성공 / Delete하기위해 id 값 조작하는 법을 배워야함

--- JS연습/API_CALL/js/step2.js
+++ JS연습/API_CALL/js/step2.js
@@ -1,7 +1,37 @@
-const API = `https://api.instantwebtools.net/v1/passenger`;
+const API = `https://api.instantwebtools.net/v1/airlines`;
 
 const _get = (target) => {
   return document.querySelector(target);
 }
 
+const _formBox = _get('.form_box');
+const _nameInput = _get('.user_name');
+const _countryInput = _get('.country');
+const _submitBtn = _get('.submit_btn');
 
+
+const handlePost = async (e) => {
+  e.preventDefault();
+  
+  const dataQuery = {
+    "name": _nameInput.value,
+    "country": _countryInput.value
+  }
+
+  try{
+    await fetch(API, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body:JSON.stringify(dataQuery)
+    })
+    console.log('성공')
+  } catch(err) {
+    console.error(err)
+  }
+  
+}
+
+
+_formBox.addEventListener('submit', handlePost)
