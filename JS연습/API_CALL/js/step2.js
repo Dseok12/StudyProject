a1c3d83aada5caece6fa41bf479dcb518f7bfbdd
D.seok <ehskstkrjsa@naver.com>
2022-03-17 16:13:53 +0900

    JS -> API_CALL / POST 셋팅

--- JS연습/API_CALL/js/step2.js
+++ JS연습/API_CALL/js/step2.js
@@ -5,34 +5,3 @@ const _get = (target) => {
 }
 
 
-const handleLoadData = async (e) => {
-  e.preventDefault();
-  
-  const address = {
-    name: _get('.user_name').value,
-    trips: _get('.country').value
-    // airline
-  }
-
-  await fetch(API, {
-    method: 'POST',
-    headers: { 'Content-type': 'application/json' },
-    body: JSON.stringify(address)
-  })
-  .then((res) => res.json())
-  .then((json) => {
-    console.log(json)
-    return console.log('데이터 보내기 성공')
-  })
-  .catch((err) =>{
-    console.error(err)
-    return console.log('데이터 전송 실패')
-  })
-
-}
-// handleLoadData();
-const init = () => {
-  _get('.submit_btn').addEventListener('submit',handleLoadData)
-}
-
-init();
