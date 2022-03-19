b0338c43cdaa6cf7fafacc7191c6c823838eb88f
D.seok <ehskstkrjsa@naver.com>
2022-03-19 14:45:55 +0900

    UX/UI -> depth3 레이아웃 완성(PC)

--- JS연습/UX_UI - 연습/01/js/index4.js
+++ JS연습/UX_UI - 연습/01/js/index4.js
@@ -1,3 +1,28 @@
+$(document).ready(function(){
 
+  $('.main_menu > li').hover(
+    function(){
+      $('.depth2_wrap').stop().slideDown()
+    },
+    function(){
+      $('.depth2_wrap').stop().slideUp()
+    }
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
 
 
