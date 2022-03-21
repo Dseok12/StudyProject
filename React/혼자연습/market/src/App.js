60e8b9f4331c7eb1275d87d27adf7f85c15680f6
D.seok <ehskstkrjsa@naver.com>
2022-03-21 19:16:44 +0900

    React -> market

--- React/혼자연습/market/src/App.js
+++ React/혼자연습/market/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Child from "./child";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>안녕하세요</h1>
+      <Child />
+      <Child />
+      <Child />
+      <Child />
     </div>
   );
 }
---
+++ React/혼자연습/market/src/child.js
@@ -0,0 +1,5 @@
+function Child () {
+  return <p>나는 자식이다.</p>
+}
+
+export default Child
