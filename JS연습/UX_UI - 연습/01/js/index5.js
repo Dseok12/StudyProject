18ee889931de1180d0fdbf060f7b269398a7e046
D.seok <ehskstkrjsa@naver.com>
2022-03-22 16:52:50 +0900

    UX/UI연습 -> X축 스크롤

--- JS연습/UX_UI - 연습/01/js/index5.js
+++ JS연습/UX_UI - 연습/01/js/index5.js
@@ -1,5 +1,12 @@
-function myFunction(event) {
-  var x = event.deltaX;
-  document.getElementById("demo").innerHTML = x;
-  console.log(x)
-}
+// function myFunction(event) {
+//   var x = event.deltaX;
+//   document.getElementById("demo").innerHTML = x;
+//   // console.log(x)
+// }
+
+const scrollContainer = document.querySelector('.box');
+
+scrollContainer.addEventListener('wheel', (evt) => {
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+});
