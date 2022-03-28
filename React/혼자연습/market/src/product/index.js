0a21bdba78658f3db216b2a285a95fdcd3c83a23
D.seok <ehskstkrjsa@naver.com>
2022-03-28 14:06:39 +0900

    React -> Market 상품 안나옴

--- React/혼자연습/market/src/product/index.js
+++ React/혼자연습/market/src/product/index.js
@@ -4,19 +4,18 @@ import { useEffect, useState } from 'react';
 
 function ProductPage () {
   const {id} = useParams();
-  const [product, setProducts] = useState(null);
+  const [product, setProduct] = useState(null);
   useEffect(function(){
-    axios.get(`https://c50bc3d7-0b3c-4a7e-98ba-182184c4fa3a.mock.pstmn.io/products/${id}`)
-    .then(
-      function(result){
-        setProducts(result.data)
-      }
-    ).catch(
-      function(error){
-        console.log(error)
-      }
+    axios.get(
+      `https://c50bc3d7-0b3c-4a7e-98ba-182184c4fa3a.mock.pstmn.io/products/${id}`
     )
-  },[])
+    .then(function(result){
+      setProduct(result.data)
+    })
+    .catch(function(error){
+      console.log(error)
+    })
+  }, [])
   console.log(product)
   return <h1>상품 {id} 상세페이지</h1>
 }
