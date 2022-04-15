08c5ad1de43d1a638580c95ab9ab5b2c2f3c4d92
D.seok <ehskstkrjsa@naver.com>
2022-04-15 14:24:44 +0900

    React: Market Front

--- React/혼자연습/market/src/upload/index.js
+++ React/혼자연습/market/src/upload/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Divider, Form, Input} from 'antd';
+import {Button, Divider, Form, Input, InputNumber} from 'antd';
 import './index.css';
 
 function UploadPage () {
@@ -7,13 +7,14 @@ function UploadPage () {
     console.log(values)
   }
   return (
-    <div>
+    <div id="upload-container">
       <Form
         name='상품 업로드'
         onFinish={onSubmit}
       >
         <Form.Item
           name='upload'
+          label={<div className="upload_label">상품이미지 등록</div>}
         >
           <div id="upload-img-placeholder">
             <img src="/images/icons/camera.png" alt="" />
@@ -22,11 +23,68 @@ function UploadPage () {
 
         </Form.Item>
         <Divider />
-        <Form.Item>
+        <Form.Item
+          label={<div
+            className="upload_label"
+          >판매자명</div>}
+          name="seller"
+          rules={[{required: true, message: '판매자이름을 입력해주세요'}]}
+        >
           <Input
+            className="upload_name"
             size="large"
             placeholder="이름을 입력해주세요"
           />
+        </Form.Item>
+        <Divider />
+        <Form.Item
+          name='name'
+          label={<div className="upload_label">상품이름</div>}
+          rules={[{required: true, message: '상품 이름을 입력해주세요'}]}
+        >
+          <Input
+            className="upload_name"
+            size="large"
+            placeholder="상품이름을 입력해주세요"
+          />
+        </Form.Item>
+        <Divider/>
+        <Form.Item
+          name="price"
+          label={<div
+            className="upload_price"
+          >상품가격</div>}
+          rules={[{required: true, message: '상품가격을 입력해주세요'}]}
+        >
+          <InputNumber
+            className="upload_price"
+            defaultValue={0}
+            size="large"
+          />
+        </Form.Item>
+        <Divider />
+        <Form.Item
+          name='description'
+          label={<div className="upload_label">상품설명</div>}
+          rules={[{required: true, message: '상품 소개를 입력해주세요'}]}
+        >
+          <Input.TextArea
+            size="large"
+            id="product-description"
+            showCount
+            maxLength={300}
+            placeholder='상품소개를 적어주세요'
+          />
+        </Form.Item>
+        <Divider/>
+        <Form.Item>
+          <Button
+            id="submit-button"
+            size="large"
+            htmlType="submit"
+          >
+            상품등록하기
+          </Button>
         </Form.Item>
       </Form>
     </div>
