8840acb437598ca853c6fbafca2bdfbef08bba31
D.seok <ehskstkrjsa@naver.com>
2022-04-15 11:41:57 +0900

    React: Alone Market

--- React/혼자연습/market/src/upload/index.js
+++ React/혼자연습/market/src/upload/index.js
@@ -1,5 +1,36 @@
+import React from "react";
+import {Divider, Form, Input} from 'antd';
+import './index.css';
+
 function UploadPage () {
-  return <h1>업로드 화면</h1>
+  const onSubmit = (values) => {
+    console.log(values)
+  }
+  return (
+    <div>
+      <Form
+        name='상품 업로드'
+        onFinish={onSubmit}
+      >
+        <Form.Item
+          name='upload'
+        >
+          <div id="upload-img-placeholder">
+            <img src="/images/icons/camera.png" alt="" />
+            <span>이미지를 업로드해주세요</span>
+          </div>
+
+        </Form.Item>
+        <Divider />
+        <Form.Item>
+          <Input
+            size="large"
+            placeholder="이름을 입력해주세요"
+          />
+        </Form.Item>
+      </Form>
+    </div>
+  )
 }
 
 export default UploadPage;
